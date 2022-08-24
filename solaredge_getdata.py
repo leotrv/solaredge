81f9c3e9c85d03aac7f74013a24d23625fcf70ae
leotrv <leo@traven.de>
2022-08-24 20:52:06 +0200

    split getdata into getfeedin and getselfcon

--- solaredge_getdata.py
+++ solaredge_getdata.py
@@ -1,26 +1,23 @@
 import requests
 import datetime
 
-feedin_priceInCent = input("Bitte Cent pro kWh angeben - Einspeisung (default: 6.53): ") or 6.53
-consumption_priceInCent = input("Bitte Cent pro kWh angeben - Eigenverbrauch (default: 25.99): ") or 25.99
-
 
 siteid = "2710924" #location
 key = "1KP4K9CZOFOS9XZU3ZXHWM24RJTXCWAL"
 
-
-#TODO: put this code into a function, problem: object of class has to have startdate,enddate from the start -> function can't be in class apicalls
-#TODO: option: insert month you wish to get the data from
 today = datetime.date.today()
 first = today.replace(day=1)
 last_month = first - datetime.timedelta(days=1)
 month =  last_month.strftime("%m")
 
+feedin_priceInEuro = (input("Bitte Cent pro kWh angeben - Einspeisung (default: 6.53): ") or 6.53)/100 #from cent to euro
+consumption_priceInEuro = (input("Bitte Cent pro kWh angeben - Eigenverbrauch (default: 25.99): ") or 25.99)/100
+month = input("Bitte gewüschten Monat angeben (default: letzter Monat): ") or month
+
 startdate="2022-" + str(month) + "-01"
 enddate="2022-" + str(month) + "-20"
 
 
-    
 #note: every time a new object of class api_calls is created, the information in the brackets is passed to the __init__ function - parameters have to be defined globally for python to put the into the init function
 
 class apicalls:
@@ -34,24 +31,33 @@ class apicalls:
         self.consumption_priceInCent = consumption_priceInCent
 
 
-    def getdata(self):
-        urldata = "https://monitoringapi.solaredge.com/site/" + self.siteid + "/energyDetails?meters=SELFCONSUMPTION,FEEDIN&timeUnit=MONTH&startTime=" + self.startdate + " 00:00:00&endTime=" + self.enddate + " 00:00:00&api_key=" + self.key #unit = MONTH, idea: import library to automatically switch to new month (with right number of days)
-        data = requests.get(urldata)
-        if data.status_code == 200:
-            data = data.json()
-            data_list = data['energyDetails']['meters']
-            feedin = data_list[0]['values'][0]['value']
-            consumption = data_list[1]['values'][0]['value'] #consumption = selfconsumption
-            feedin_money = feedin/1000 * float(feedin_priceInCent)/100 #from Wh to kWh, from euro to ct
-            consumption_money = consumption/1000 * float(consumption_priceInCent/100) 
+    def getfeedin(self):
+        urlfeedin = "https://monitoringapi.solaredge.com/site/" + self.siteid + "/energyDetails?meters=FEEDIN&timeUnit=MONTH&startTime=" + self.startdate + " 00:00:00&endTime=" + self.enddate + " 00:00:00&api_key=" + self.key #unit = MONTH, idea: import library to automatically switch to new month (with right number of days)
+        feedin = requests.get(urlfeedin)  
+        if feedin.status_code == 200:
+            feedin = feedin.json()
+            feedin_list = feedin['energyDetails']['meters']
+            feedin = (feedin_list[0]['values'][0]['value'])/1000 #from Wh to kWh 
+            feedin_money = feedin * float(feedin_priceInEuro) #from euro to ct
             print("Deine Einnahmen durch die Einspeisung für den abgelaufenen Monat betragen: ", round(feedin_money,2), "€")
+            print("Du hast ",round(feedin,2)," kWh eingespeist!")
+
+    def getselfcon(self):
+        urlselfcon = "https://monitoringapi.solaredge.com/site/" + self.siteid + "/energyDetails?meters=SELFCONSUMPTION&timeUnit=MONTH&startTime=" + self.startdate + " 00:00:00&endTime=" + self.enddate + " 00:00:00&api_key=" + self.key #unit = MONTH, idea: import library to automatically switch to new month (with right number of days)
+        selfcon = requests.get(urlselfcon)
+        if selfcon.status_code == 200:
+            selfcon = selfcon.json()
+            selfcon_list = selfcon['energyDetails']['meters']
+            selfcon = (selfcon_list[0]['values'][0]['value'])/1000 #consumption = selfconsumption
+            consumption_money = selfcon * float(consumption_priceInEuro)
             print("Deine Kosten für den Eigenverbrauch für den abgelaufenen Monat betragen: ", round(consumption_money,2), "€")
-            
-            
+            print("Du hast ",round(selfcon,2)," kWh selber verbraucht!")
 
 
-testobject = apicalls(siteid,key,startdate,enddate,feedin_priceInCent, consumption_priceInCent)
-testobject.getdata()
+
+accounting = apicalls(siteid,key,startdate,enddate,feedin_priceInEuro, consumption_priceInEuro)
+accounting.getselfcon()
+accounting.getfeedin()
 
 
 
