968a84ddc375fdacff16de718fe84ada95dcea6f
leotrv <leo@traven.de>
2022-08-29 19:52:59 +0200

    simplify consumption and feedin price input

--- solaredge_getdata.py
+++ solaredge_getdata.py
@@ -5,30 +5,35 @@ import datetime
 siteid = "2710924" #location
 key = "1KP4K9CZOFOS9XZU3ZXHWM24RJTXCWAL"
 
+
 today = datetime.date.today()
 first = today.replace(day=1)
 last_month = first - datetime.timedelta(days=1)
 month =  last_month.strftime("%m")
 
-feedin_priceInEuro = (input("Bitte Cent pro kWh angeben - Einspeisung (default: 6.53): ") or 6.53)/100 #from cent to euro
-consumption_priceInEuro = (input("Bitte Cent pro kWh angeben - Eigenverbrauch (default: 25.99): ") or 25.99)/100
-month = input("Bitte gewüschten Monat angeben (default: letzter Monat): ") or month
 
+consumption_priceInEuro = input("Bitte Cent pro kWh angeben - Eigenverbrauch (default: 25.99): ")
+feedin_priceInEuro = input("Bitte Cent pro kWh angeben - Einspeisung (default: 6.53): ")
+consumption_priceInEuro = (float(consumption_priceInEuro.replace(",",".")) if type(consumption_priceInEuro) == str and "," in consumption_priceInEuro else 25.99)/100
+feedin_priceInEuro = (float(feedin_priceInEuro.replace(",",".")) if type(feedin_priceInEuro) == str and "," in feedin_priceInEuro else 6.53)/100
+
+
+month = input("Bitte gewüschten Monat angeben (default: letzter Monat): ") or month
 startdate="2022-" + str(month) + "-01"
-enddate="2022-" + str(month) + "-20"
+enddate="2022-" + str(month) + "-20" #any other day works as the unit is month anyway
 
 
 #note: every time a new object of class api_calls is created, the information in the brackets is passed to the __init__ function - parameters have to be defined globally for python to put the into the init function
 
 class apicalls:
 
-    def __init__(self, siteid, key, startdate, enddate, feedin_priceInCent, consumption_priceInCent):
+    def __init__(self, siteid, key, startdate, enddate, feedin_priceInEuro, consumption_priceInEuro):
         self.siteid = siteid
         self.key = key
         self.startdate = startdate
         self.enddate = enddate
-        self.feedin_priceInCent = feedin_priceInCent
-        self.consumption_priceInCent = consumption_priceInCent
+        self.feedin_priceInCent = feedin_priceInEuro
+        self.consumption_priceInCent = consumption_priceInEuro
 
 
     def getfeedin(self):
